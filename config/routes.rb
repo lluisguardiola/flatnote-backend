ecdb351fcdaae0be427a3fe07a69da6b06f38366
lluisguardiola <lluisguardiola.h@gmail.com>
2020-02-09 18:14:34 -0600

    controllers & resources initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users [except: :index]
+  resources :notes [except: :index]
+  resources :tags [except: :index]
 end
