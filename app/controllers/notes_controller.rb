84fd880347ff4694e88f037701706cf1d6f25346
lluisguardiola <lluisguardiola.h@gmail.com>
2020-02-15 17:04:17 -0600

    create Note controller & route setup

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,13 @@
 class NotesController < ApplicationController
-  def index
+
+  def create
+    note = Note.new(user_id: params[:user_id], title: params[:title], content: params[:content])
+    if note.valid?
+      note.save
+      render json: note
+    else
+      render json: {error: 'note could not be created'}
+    end
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 	resources :users, only: [:index, :create, :show]
-	resources :notes, only: [:index]
+	resources :notes, only: [:create]
 	resources :tags, only: [:index]
 
+	
 	post '/auth', to: 'auth#create'
 	get '/current_user', to: 'auth#show'
 end
